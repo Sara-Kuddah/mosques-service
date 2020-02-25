d1fa0778f900674845b89b83bfba675878f0ee73
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-02-25 13:35:05 +0300

    done delete

--- src/Item.js
+++ src/Item.js
@@ -7,23 +7,30 @@ export default class Item extends React.Component{
      constructor(props){
          super(props);
          this.state ={
-          data:this.props.data
+          elemint:this.props.elemint
         };
      }
-
+   
+     /*
+      onDeleteButtonClick = (e) =>
+        this.props.deleteItem(this.props.elemint.name)
+     */
      
      render(){
        return(
+         <div>
+          <button onClick={()=>this.props.deleteItem(this.state.elemint)}>Delete This Item</button>
         <div  className="block">
+          
         <div className="blockElemM">
-        <MapContainer name={this.props.elemint.name} lat={this.props.elemint.geometry.location.lat}  lng={this.props.elemint.geometry.location.lng}/>
+        <MapContainer name={this.state.elemint.name} lat={this.state.elemint.geometry.location.lat}  lng={this.state.elemint.geometry.location.lng}/>
         </div>
 
         <div className="blockElemT"  >
-        <Detals name={this.props.elemint.name} comment={this.props.elemint.comment} url= {this.props.elemint.url}/>
+        <Detals name={this.state.elemint.name} comment={this.state.elemint.comment} url= {this.state.elemint.url}/>
         </div>
 
-      
+        </div>
         </div>
        );
 
--- src/ListContainer.js
+++ src/ListContainer.js
@@ -3,14 +3,29 @@ import './App.css';
 import Item from './Item';
 
 export default class ListContainer extends React.Component{
-     
-     render(){
-            const allItem=this.props.data.map( function (elemint ,index){
-              return <Item key={index} elemint={elemint}/>;
-            });
+  constructor(props){
+    super(props);
+    this.state ={
+     data:this.props.data
+   };
+  }
+ deleteItem = (e) => {
+   console.log("Item Deleted");
+    const deleted = this.state.data.filter(i => i!==e)
+    console.log('dd', deleted)
+    this.setState({
+      data:deleted
+    });
+  }
 
+     render(){
+            const allItem=this.state.data.map((elemint ,index) => {
+              return <Item key={index} elemint={elemint} deleteItem={this.deleteItem}/>;
+            });
+        console.log(this.state.data);
        return(
          <div>
+           
             {allItem}
          </div>
        );
