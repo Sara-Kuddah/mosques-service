5459df76477c7fc12313e15d899a7873cffa4c42
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-02-26 16:06:38 +0300

    Updates

---
+++ precache-manifest.2c96ef03c7ae78e304aa411ef8f6c743.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f275d9ed99bf7716329ab03a27c5c452",
+    "url": "/mosques-service/index.html"
+  },
+  {
+    "revision": "fad54cbf84a51b03db26",
+    "url": "/mosques-service/static/css/main.85dfda31.chunk.css"
+  },
+  {
+    "revision": "3c0704412cdd74cf5122",
+    "url": "/mosques-service/static/js/2.0ae9d60a.chunk.js"
+  },
+  {
+    "revision": "959150e688866e3de05cb62dc4dcd7db",
+    "url": "/mosques-service/static/js/2.0ae9d60a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fad54cbf84a51b03db26",
+    "url": "/mosques-service/static/js/main.1554aa6d.chunk.js"
+  },
+  {
+    "revision": "bbb86fb0dc7863ebd10b",
+    "url": "/mosques-service/static/js/runtime-main.06c6bda3.js"
+  }
+]);
